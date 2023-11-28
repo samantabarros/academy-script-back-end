a088e383e834044b8806d1a48b7870b8a3967088
samantabarros <samantadesenvolvedora@gmail.com>
2023-11-27 22:42:57 -0300

    tarefa(auth): criação de modulo, service e controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { AlunosModule } from './modules/alunos/alunos.module';
 import { ModulosModule } from './modules/modulos/modulos.module';
 import { MatriculaModule } from './modules/matricula/matricula.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuariosModule, AlunosModule, ModulosModule, MatriculaModule],
+  imports: [UsuariosModule, AlunosModule, ModulosModule, MatriculaModule, AuthModule],
   controllers: [],
   providers: [],
 })
